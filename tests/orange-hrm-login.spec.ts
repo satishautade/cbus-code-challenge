2783984054ce725615195b1286ba3e2884d093ee
Satish Autade <satishautade@gmail.com>
2025-02-26 19:11:51 +1100

    Add Failed login test

--- tests/orange-hrm-login.spec.ts
+++ tests/orange-hrm-login.spec.ts
@@ -1,17 +1,24 @@
-import { test, expect, Page } from '@playwright/test';
-import { LoginPage  } from './pages/login-page';
+import { test as base, expect, Page } from '@playwright/test';
+import { LoginPage } from './pages/login-page';
 import { DashboardPage } from './pages/dashboard-page';
 
-test.beforeEach(async ({ page }) => {
-  const loginPage  = new LoginPage(page);
-  await loginPage.goto();
+// Extend the test with a fixture to share the LoginPage instance
+const test = base.extend<{ loginPage: LoginPage }>({
+  loginPage: async ({ page }, use) => {
+    const loginPage = new LoginPage(page);
+    await loginPage.goto();
+    await use(loginPage);
+  },
 });
 
-test('Orange HRM: Successful Login test', async ({ page }) => {
-
-  const loginPage  = new LoginPage(page);
+test('Orange HRM: Successful Login test', async ({ loginPage, page }) => {
   await loginPage.login('Admin', 'admin123');
   const dashboardPage = new DashboardPage(page);
-  await expect(dashboardPage.isCurrentPage()).toBeTruthy();
+  await expect(await dashboardPage.isCurrentPage()).toBeTruthy();
+});
 
+test('Orange HRM: Failed Login test', async ({ loginPage, page }) => {
+  await loginPage.login('wrongUser', 'wrongPassword');
+  const dashboardPage = new DashboardPage(page);
+  await expect(await dashboardPage.isCurrentPage()).toBeFalsy();
 });
